036af16826098477ae946e5ae0787c90b03cba1e
shexrozzz_mamanov <shexroz81@gmail.com>
2025-10-12 18:24:43 +0500

    You Tube Projectda saytga Main bolimi qoshildi

--- YouTube-project/js/app.js
+++ YouTube-project/js/app.js
@@ -1,7 +1,5 @@
 const toggleBtn = document.getElementById('toggleSidebar');
 const sidebar = document.querySelector('.sidebar');
-const items = sidebar.querySelectorAll("li");
-click = 0;
 
 toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("hide-text");
