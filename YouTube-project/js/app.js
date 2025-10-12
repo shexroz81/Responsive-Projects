03513a53788942c03c8f545712d210e0d61d8d8c
shexrozzz_mamanov <shexroz81@gmail.com>
2025-10-12 16:24:03 +0500

    You Tube Projectda saytga Js orqali toggle function qoshildi

--- YouTube-project/js/app.js
+++ YouTube-project/js/app.js
@@ -1,1 +1,10 @@
-// alert("Xush Kelibsiz YouTubega")
+const toggleBtn = document.getElementById('toggleSidebar');
+const sidebar = document.querySelector('.sidebar');
+const items = sidebar.querySelectorAll("li");
+click = 0;
+
+toggleBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("hide-text");
+    sidebar.classList.toggle("collapsed");
+})
+
